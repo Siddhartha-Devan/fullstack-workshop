288f13cf1109468a443ea0234754c7506e9a9256
Siddharth <siddu2003oct@gmail.com>
2023-08-03 11:31:14 +0530

    js datatypes

---
+++ app.js
@@ -0,0 +1,34 @@
+// var a = 10
+// console.log("This ia value in var : ", a)
+
+// {
+//     var a = 6
+//     console.log("after block altering : ", a)
+// }
+
+// var a = ["d", "d"]7
+// console.log("after type-casting : ", a)
+
+
+// let b = 5
+// console.log("This is a let value : ", b)
+
+// const c = "krgeh"
+// console.log("This is a const value : ", c)
+
+
+
+let a = 5
+console.log(a)
+
+{
+    let a =7
+    console.log(a)
+}
+
+// let a = 10
+console.log(a)
+
+{
+    console.log(a)
+}
