1229d8e8c0a47293e407a6019faec8a27c3187fa
Siddharth <siddu2003oct@gmail.com>
2023-08-03 15:16:48 +0530

    json

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+
 // var a = 10
 // console.log("This ia value in var : ", a)
 
@@ -45,21 +46,65 @@
 // }
 
 //while loop
-i = 0
-while(i<10) {
-    console.log(i)
-    i++
+// i = 0
+// while(i<10) {
+//     console.log(i)
+//     i++
+// }
+
+// console.log("\n")
+
+// for (i = 0; i <10; i++) {
+//     console.log(i)
+// }
+
+// console.log("\n")
+
+// l1 = [10,20,30,40]
+// l1.forEach(i => {
+//     console.log(i)
+// })
+
+// const s = [1, 2, 3, 4, 5, "g", "h", "i"]
+// s.push(10)
+// s.push('S')
+// console.log(s)
+
+// for (i in s) {
+//     console.log(s[i])
+// }
+// console.log("length = ", s.length)
+// for (i = 0; i < 10; i++) {
+//     s.pop()
+// }
+
+// console.log("after popping len = ", s.length)
+// console.log("after popping", s)
+
+// s.push("sidd")
+// s.push('vettri')
+// console.log(s)
+
+
+car_v = {
+    brand : "toyota",
+    model : "Object",
+    year: 2010,
+    sunroof : {is_p : "no", shape : "adhaan illaye"}
+}
+// car_v.cam = "no"
+// console.log(car_v)
+// console.log(car_v.length)
+
+for (i in car_v) {
+    if (typeof car_v[i] === "object") {
+        console.log("yes")
+        for (j in car_v[i]) {
+            console.log(car_v[i][j])
+        }
+    }
+    else {
+    console.log("else", car_v[i])
+    }
 }
 
-console.log("\n")
-
-for (i = 0; i <10; i++) {
-    console.log(i)
-}
-
-console.log("\n")
-
-l1 = [10,20,30,40]
-l1.forEach(i => {
-    console.log(i)
-})
